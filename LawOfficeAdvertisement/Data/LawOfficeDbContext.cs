5e0ee8bcd4f18f6bcc1b964c3c38e5a3639ec989
NikkiIV <prince_the_ripper@abv.bg>
2023-08-02 12:30:14 +0300

    Initial commit

---
+++ LawOfficeAdvertisement/Data/LawOfficeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LawOfficeAdvertisement.Data
+{
+    public class LawOfficeDbContext : IdentityDbContext
+    {
+        public LawOfficeDbContext(DbContextOptions<LawOfficeDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
